ff7d9d62afd0f391097e7c5751ca1072227734a6
desousal1 <desousal1@BY2024A98603995.wit.private>
2021-03-22 15:45:53 -0400

    Added test code for creating and drawing from deck

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,7 +2,9 @@ package application;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.print("Hello World!");
-		System.out.print("Hello World!");
+		Deck test = new Deck();
+		System.out.println(test.cards);
+		System.out.println(test.draw());
+		System.out.println(test.cards);
 	}
 }

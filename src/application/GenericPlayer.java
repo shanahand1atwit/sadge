c60b3c6760dfd9c003738a769e4d7ea1c8f6bc1b
desousal1 <desousal1@BY2024A98603995.wit.private>
2021-03-22 00:34:57 -0400

    Added comments for variables and constructor

--- src/application/GenericPlayer.java
+++ src/application/GenericPlayer.java
@@ -1,14 +1,20 @@
 package application;
 
 public class GenericPlayer {
-	private String name;
-	private String color;
-	private Pawns[] playerPawns = new Pawns[4];
+	private String name;	// holds player's name
+	private String color;	// hold color of player
+	private Pawns[] playerPawns = new Pawns[4];	// holds player's four pawns
 
+	/**
+	 * Initializes GenericPlayer
+	 * 
+	 */
 	public GenericPlayer(String nm, String clr)
 	{
 		name = nm;
 		color = clr;
+		
+		// for loop creates 4 pawns for player
 		for (int x =0; x<playerPawns.length; x++)
 		{
 			playerPawns[x] = new Pawns(color);

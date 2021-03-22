496919b53510eedb1b69649b15c55acaa5cb4f97
desousal1 <desousal1@BY2024A98603995.wit.private>
2021-03-22 15:44:03 -0400

    Increased amount of tiles and added comments for tile ranges

--- src/application/Board.java
+++ src/application/Board.java
@@ -1,7 +1,11 @@
 package application;
 
 public class Board {
-	public Tiles[] boardArray = new Tiles[64];	// array holds all tiles on board
+	public Tiles[] boardArray = new Tiles[88];	// array holds all tiles on board
+												// 0-59 Tiles around board
+												// 60-79 Safety Zones
+												// 80-83 Start
+												// 84-87 Home
 	
 	public Board()
 	{

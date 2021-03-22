ca2862eb3d5c345f59c4c7a94998efba9a428b3e
desousal1 <desousal1@BY2024A98603995.wit.private>
2021-03-22 00:34:21 -0400

    Added comment for array

--- src/application/Board.java
+++ src/application/Board.java
@@ -1,7 +1,7 @@
 package application;
 
 public class Board {
-	public Tiles[] boardArray = new Tiles[64];
+	public Tiles[] boardArray = new Tiles[64];	// array holds all tiles on board
 	
 	public Board()
 	{

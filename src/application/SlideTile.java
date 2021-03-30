f83e83f1f966d30da569b34f82ff6840f7c7aace
desousal1 <desousal1@BY2024A98603995.wit.private>
2021-03-30 14:29:37 -0400

    Added code for SlideTile

--- src/application/SlideTile.java
+++ src/application/SlideTile.java
@@ -1,9 +1,22 @@
 package application;
 
 public class SlideTile extends Tiles{
-
+	
+	private boolean isStart = false;
+	private int length;
+	
 	public SlideTile(int tn, String color) {
 		super(tn, color);
+		if (tn == 1 || tn == 16 || tn == 31 || tn == 46)
+		{
+			isStart = true;
+			length = 4;
+		}
+		else if (tn == 9 || tn == 24 || tn == 39 || tn == 54)
+		{
+			isStart = true;
+			length = 5;
+		}
 	}
 
 }
